2765ed76eb5d5dfefe7c44164c51b77f85f27da6
Abdoul Idriss <Abdoulben44@gmail.com>
2024-07-28 05:13:47 +0100

    modif for cors

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,12 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    credentials: true,
-    origin:['http://localhost:4200']
-}));
+const corsOptions ={
+    origin:'http://localhost:4200', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const MONGO_URL = 'mongodb://localhost:27017/employee';
 mongoose.connect(MONGO_URL, {
